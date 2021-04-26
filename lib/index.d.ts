cdded8c1d9832023c5507a131150e0a864437a6e
Chris <chris.barwick16@gmail.com>
2021-04-26 15:04:36 -0700

    Implement interface Opts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,23 @@
 export function fromStringV2(
-    opts: Array<String>,
+    opts: Opts,
     clbk: (err: Error | null, body: Record<string, any>) => void,
     dest: string | undefined
 ): void;
 
 export function fromFileV2(
     srcFile: string,
-    opts: Array<String>,
+    opts: Opts,
     clbk: (err: Error | null, body: Record<string, any>) => void,
     dest: string | undefined
 ): void;
+
+interface Opts {
+    compiler: string,
+    code: string,
+    save: boolean,
+    codes?: Array<string>,
+    options?: string,
+    stdin?: string,
+    "compiler-option-raw"?: string,
+    "runtime-option-raw"?: string,
+}
