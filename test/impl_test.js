1d0254fec4857c10d9eee7a6573bda8ae702dcce
Chris <chris.barwick16@gmail.com>
2021-05-04 09:48:56 -0700

    Fixed JSON.parse SyntaxError Fixed String Dest required when saving

--- test/impl_test.js
+++ test/impl_test.js
@@ -5,8 +5,9 @@ let res = runWandbox.fromStringV2(
 		compiler: "lua-5.4.0",
 		codes: [],
 		options: "",
-		save: false,
-		timeout: 30000
+		save: true,
+		timeout: 30000,
+		code: "print(\"Hello\")"
 	},
 	function done(error, res) {
 		console.log(error);
